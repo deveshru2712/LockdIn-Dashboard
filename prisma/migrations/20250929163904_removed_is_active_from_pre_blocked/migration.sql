0cd8734f844ab2c6440cc30c871bddb77edc7f19
Devesh Chandra <deveshru2712@gmail.com>
2025-09-29 22:13:43 +0530

    added pre defined config for the available option of website to block

---
+++ prisma/migrations/20250929163904_removed_is_active_from_pre_blocked/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isActive` on the `predefined_websites` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."predefined_websites" DROP COLUMN "isActive";
